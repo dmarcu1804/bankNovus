6fde5521455e1248f7c734168a8735ec4815a158
Dragos <dragos@Dragoss-MacBook-Pro.local>
2021-04-15 14:55:10 +0100

    login page added; currently have sign up, database and login

--- NovusBank/src/main/java/hibernateDao/UserDao.java
+++ NovusBank/src/main/java/hibernateDao/UserDao.java
@@ -23,5 +23,30 @@ public class UserDao {
             e.printStackTrace();
         }
     }
+    
+    public boolean validate(String email, String password) {
+
+        Transaction transaction = null;
+        User user = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // get an user object
+            user = (User) session.createQuery("FROM User U WHERE U.email = :email").setParameter("email", email)
+                .uniqueResult();
+
+            if (user != null && user.getPassword().equals(password)) {
+                return true;
+            }
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
 
--- NovusBank/src/main/java/hibernateUtil/HibernateUtil.java
+++ NovusBank/src/main/java/hibernateUtil/HibernateUtil.java
@@ -23,14 +23,14 @@ public class HibernateUtil {
                 settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/testdb?useSSL=false");
                 settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "YourPassword");
+                settings.put(Environment.PASS, "Your Password");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
                 settings.put(Environment.SHOW_SQL, "true");
 
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
 
                 configuration.setProperties(settings);
                 configuration.addAnnotatedClass(User.class);
